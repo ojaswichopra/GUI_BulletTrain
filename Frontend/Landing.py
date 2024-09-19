b78986ba9b7bfa034924e244032d995c8e3b3185
Ojaswi Chopra <ojaswichopra06@gmail.com>
2024-09-19 15:30:01 +0530

    Last fix

--- Frontend/Landing.py
+++ Frontend/Landing.py
@@ -68,10 +68,10 @@ def main():
             st.write("Button 1 clicked!")
         if st.button('See output'):
             oc = Oct2Py()
-            oc.eval('cd("../backend\\ codes")')
+            oc.eval('cd("../backend_codes")')
             oc.eval(f"trial({1},{2})")
             print("Load flow executed successfully!")
-            if os.path.isfile("../backend codes/trail.mat"):
+            if os.path.isfile("../backend_codes/trail.mat"):
                 st.write(f"File exists.")
             else:
                 st.write(f"No.")
