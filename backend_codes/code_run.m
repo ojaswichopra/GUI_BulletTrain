6c4d923bc83542064ea2178c03bd2dc82f294315
Ojaswi Chopra <ojaswichopra06@gmail.com>
2024-10-10 14:58:24 +0530

    Added Progress Bar

--- backend_codes/code_run.m
+++ backend_codes/code_run.m
@@ -2,8 +2,10 @@ function code_run(TSS, AT, N, N_hr,train_time, Vp, Vs, Rp, Xp, Rs, Xs, Zn, short
     
     tic
     format longG
-    global TSS, AT, N, N_hr,train_time, Vp, Vs, Rp, Xp, Rs, Xs, Zn, short_ckt_MVA, lresistance, lreactance, mresistance, mreactance, row, f, n1, Ch, Mh, nFh, nFd, gh, gd, contact_wire_diameter, Resistance_C, messenger_wire_diameter, Resistance_M1, earth_wire_diameter, Resistance_G, feeder_wire_diameter, Resistance_F, rail_diameter, Resistance_R1
 
+    % Create a file to store the progress
+    progress_file = 'progress_file.txt';
+    
     Line_data_bus_data_bus_type_normal;        %% calling for data creaction for whole traction network from TSS1 to TSS11%%
     separate_line_data;
     HSR;                    %%loading of data file%%
@@ -22,6 +24,11 @@ function code_run(TSS, AT, N, N_hr,train_time, Vp, Vs, Rp, Xp, Rs, Xs, Zn, short
         output_display;             %%display of result%%
         for_display_upload;
         percentage_completion_3=(nt*100)/NT   %percentage of total computation completed
+        % Write progress to the file using fopen
+        % Open the file for writing
+        fid = fopen(progress_file, 'w');
+        fprintf(fid, '%.2f\n', percentage_completion_3);  % Write progress to file with seven decimal places
+        fclose(fid);
     end
     TSS_MVA_voltage_unbalance_source_code;
     AT_MVA_source_code;
