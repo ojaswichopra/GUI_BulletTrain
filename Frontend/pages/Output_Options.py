c891b850264d9ff31f0dfab03165024a28b78bbe
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2024-10-20 13:45:14 +0530

    EN-50641 standard load flow complete

--- Frontend/pages/Output_Options.py
+++ Frontend/pages/Output_Options.py
@@ -3,9 +3,9 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 from oct2py import Oct2Py
 import os
 from pages.workspace import workspace_variables
+from pages.EN_workspace import en_workspace
 import numpy as np
-oc = Oct2Py() 
-oc.eval('cd("../EN-50641")') 
+ 
 
 def read_text_file(file_path):
     try:
@@ -101,9 +101,28 @@ def load_workspace_variables():
 
 
 def load_EN_workspace():
+    oc = Oct2Py() 
+    oc.eval('cd("../EN-50641")')
     #steps:
     #first load the .mat file using oc.. then push the variables from oc to workspace
-    oc.load('required_variable_load_flow_standard.mat')
+    try:
+        # Attempt to load the .mat file
+        oc.eval('load("required_variable_load_flow_standard.mat")')
+        print("MAT file loaded successfully.") 
+        variables_in_workspace = oc.eval('who')  # This returns all variable names
+        print(variables_in_workspace)  # Print out all available variables
+    except Oct2PyError as e:
+        print(f"Error loading MAT file: {e}")
+        return  # Exit the function if the file can't be loaded
+    
+    
+    EN_variable_names = [
+        'HS_train_A_F_data','HS_train_F_A_data','FR_train_A_F_data','SUB_train_A_F_data','d','line_data_M_emp','dTSS','y_whole','y','Vc_mag_Td','Vc_ang_Td','VR_mag_Td','VR_ang_Td','pTSS_T','Vf_mag_Td','sub_station_catenary_current','sub_station_feeder_current'
+    ]
+    for var in EN_variable_names:
+        my_var = oc.pull(var)
+        en_workspace[var] = my_var
+
 
 
 
@@ -158,7 +177,7 @@ def main():
     with col2:
         if st.button('Output of EN-50641 standard'):
             load_EN_workspace()
-            st.switch_page("")
+            st.switch_page("pages/EN_Output.py")
 
 if __name__ == "__main__":
     main()
