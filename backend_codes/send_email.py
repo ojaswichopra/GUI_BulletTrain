07a38ad1e6858daf2d0b5d79f8e32af51c255717
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2024-11-04 00:54:30 +0530

    email feature updated and fixed..

--- backend_codes/send_email.py
+++ backend_codes/send_email.py
@@ -4,13 +4,10 @@ from email.mime.text import MIMEText
 import os
 import sys
 # Add the frontend directory to the sys.path
-frontend_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../Frontend'))
-sys.path.insert(0, frontend_path)
 
+with open('../variable_text_files/email.txt', 'r') as file:
+    recipient = file.read().strip()
 
-
-from pages.run_loadflow import email_recip
-# print(dir(pages.Execute_Load_Flow))
 
 def send_email():
     sender = "guibullettrain@gmail.com"
@@ -21,13 +18,13 @@ def send_email():
     msg = MIMEText('Load Flow has been executed successfully. You can now see output of load flow.')
     msg["Subject"] = 'Load Flow Executed'
     msg["From"] = sender
-    msg["To"] = email_recip
+    msg["To"] = recipient
 
     try:
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
         server.login(sender, password)
-        server.sendmail(sender, email_recip, msg.as_string())
+        server.sendmail(sender, recipient, msg.as_string())
         server.quit()
         print("Email sent successfully")
     except Exception as e:
