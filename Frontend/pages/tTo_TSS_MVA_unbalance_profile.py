4b79e6c746dfa3d644a1416159593a40916a8705
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2024-11-27 10:31:51 +0530

    Third output section completed

---
+++ Frontend/pages/tTo_TSS_MVA_unbalance_profile.py
@@ -0,0 +1,153 @@
+import os
+import streamlit as st
+from PIL import Image
+# Create an Oct2Py instance once
+from pages.tTo_Workspace import tTo_workspace
+from streamlit_extras.add_vertical_space import add_vertical_space
+from oct2py import Oct2Py
+import pandas as pd
+oc = Oct2Py() 
+oc.eval('cd("../two_TSS_outage")') 
+
+
+
+
+def main():
+
+    st.markdown(
+            """
+        <style>
+            [data-testid="collapsedControl"] {
+                display: none
+            }
+            .title {
+                text-align: center;
+            }
+        </style>
+        """,
+            unsafe_allow_html=True,
+        )
+
+    st.markdown("<h1 class='title'>TSS MVA & Voltage Unbalance profile</h1>", unsafe_allow_html=True)
+
+    nn_track = st.number_input("Enter the TSS number for MVA profile", min_value=0)
+
+    if(st.button("Show TSS MVA profile")):
+        oc.eval("setenv('GNUTERM', 'gnuplot')")
+
+        TSS = tTo_workspace['TSS']
+        oc.push('TSS', TSS)
+        N_TSS_O = tTo_workspace['N_TSS_O']
+        oc.push('N_TSS_O', N_TSS_O)
+        dTSS_T = tTo_workspace['dTSS_T']
+        oc.push('dTSS_T', dTSS_T)
+        s_apprant_power_MVA_mag = tTo_workspace['s_apprant_power_MVA_mag']
+        oc.push('s_apprant_power_MVA_mag', s_apprant_power_MVA_mag)
+        # Mva_sec_abs = tTo_workspace['Mva_sec_abs']
+        # oc.push('Mva_sec_abs', Mva_sec_abs)
+        Unb = tTo_workspace['Unb']
+        oc.push('Unb', Unb)
+        tt_time = tTo_workspace['tt_time']
+        oc.push('tt_time', tt_time)
+
+        oc.eval(f"TSS_MVA_voltage_unbalance_profile_plot_outage_load_txt({nn_track},TSS,N_TSS_O,dTSS_T,s_apprant_power_MVA_mag,Unb,tt_time)")
+        # maximum_mva = result[0], maximum_unbalance = result[1]
+        # maximum_mva, maximum_unbalance = oc.feval(
+        #     "TSS_MVA_voltage_unbalance_profile_plot_outage_load_txt", 
+        #     TSS, N_TSS_O, dTSS_T, s_apprant_power_MVA_mag, Unb, tt_time
+        # )
+        image_path = '../Plots_tTo/tTo_TSS_MVA_voltage_unbalance.png'
+        img = Image.open(image_path)
+        st.image(img, caption="", use_column_width=True)
+
+        with open(image_path, "rb") as file:
+            btn = st.download_button(
+                label="Download Plot",
+                data=file,
+                file_name="tTo_TSS_MVA_voltage_unbalance.png",  # Replace with the desired download filename
+                mime="image/png"
+            )
+    if os.path.isfile('../two_TSS_outage/maximum_mva_and_unbalance.mat'):
+
+        oc.eval(f"load('maximum_mva_and_unbalance.mat')")
+        col1, col2 = st.columns(2)
+        maximum_mva = oc.pull('maximum_mva')
+        maximum_unbalance = oc.pull('maximum_unbalance')
+        with col1:
+            if(st.button("Maximum MVA of all TSS")):
+                
+                flattened_maximum_mva = maximum_mva[0]
+                maximum_mva_df = pd.DataFrame({
+                    "Maximum MVA" : flattened_maximum_mva
+                })
+                # print(maximum_mva_df)
+                maximum_mva_df.index = range(1, len(maximum_mva_df)+1)
+                maximum_mva_df.rename_axis("TSS Number", inplace=True)
+                add_vertical_space(2)
+                styled_table1 = maximum_mva_df.style.set_table_attributes('style="margin:auto; width:80%;"')\
+                                .set_table_styles([{
+                                    'selector': 'th',
+                                    'props': [('text-align', 'center')]
+                                }, {
+                                    'selector': 'td',
+                                    'props': [('text-align', 'center')]
+                                }])
+                
+                # Display the table with center-aligned text
+                st.markdown("<div class='table-container'>", unsafe_allow_html=True)  # Centering the table
+                st.dataframe(styled_table1, use_container_width=True)
+                st.markdown("</div>", unsafe_allow_html=True)
+
+                # Add a download button for the DataFrame
+                csv = maximum_mva_df.to_csv(index=True).encode('utf-8')
+                st.markdown("<div class='download-button'>", unsafe_allow_html=True)
+                st.download_button(
+                    label="Download Maximum MVA Values as CSV",
+                    data=csv,
+                    file_name='maximum_MVA_tss.csv',
+                    mime='text/csv',
+                    key='download-csv-1'
+                )
+                st.markdown("</div>", unsafe_allow_html=True)
+        with col2: 
+            if(st.button("Maximum Voltage Unbalance of all TSS")):
+                
+                flattened_maximum_unbalance = maximum_unbalance[0]
+                maximum_unbalance_df = pd.DataFrame({
+                    "Maximum voltage unbalance (%)" : flattened_maximum_unbalance
+                })
+
+                maximum_unbalance_df.index = range(1, len(maximum_unbalance_df)+1)
+                maximum_unbalance_df.rename_axis("TSS Number", inplace=True)
+                add_vertical_space(2)
+                styled_table2 = maximum_unbalance_df.style.set_table_attributes('style="margin:auto; width:80%;"')\
+                                .set_table_styles([{
+                                    'selector': 'th',
+                                    'props': [('text-align', 'center')]
+                                }, {
+                                    'selector': 'td',
+                                    'props': [('text-align', 'center')]
+                                }])
+                
+                # Display the table with center-aligned text
+                st.markdown("<div class='table-container'>", unsafe_allow_html=True)  # Centering the table
+                st.dataframe(styled_table2, use_container_width=True)
+                st.markdown("</div>", unsafe_allow_html=True)
+
+                # Add a download button for the DataFrame
+                csv = maximum_unbalance_df.to_csv(index=True).encode('utf-8')
+                st.markdown("<div class='download-button'>", unsafe_allow_html=True)
+                st.download_button(
+                    label="Download Maximum Unbalance Values as CSV",
+                    data=csv,
+                    file_name='maximum_unbalance_tss.csv',
+                    mime='text/csv',
+                    key='download-csv-2'
+                )
+                st.markdown("</div>", unsafe_allow_html=True)
+
+
+if __name__ == "__main__":
+    main()
+    if st.button("Back"):
+        st.switch_page("pages/tTo_output_options.py")
--- two_TSS_outage/TSS_MVA_voltage_unbalance_profile_plot_outage_load_txt.m
+++ two_TSS_outage/TSS_MVA_voltage_unbalance_profile_plot_outage_load_txt.m
@@ -1,53 +1,64 @@
-clc
-clear all
-load("TSS.txt");
-load("N_TSS_O.txt");
-load("dTSS_T.txt");
-load("s_apprant_power_MVA_mag.txt");
-load("Mva_sec_abs.txt");
-load("Unb.txt");
-load("tt_time.txt");
-nn_track=input("Enter the TSS number for MVA profile");
-track_TSS_no=length(TSS)-length(N_TSS_O);           %% total no of TSS in track
-ntt=length(dTSS_T(:,1));            %% total time interval of whole day operation
-nt_track=1;
-for n_track=1:1:track_TSS_no+2
-   
+function TSS_MVA_voltage_unbalance_profile_plot_outage_load_txt(nn_track,TSS,N_TSS_O,dTSS_T,s_apprant_power_MVA_mag,Unb,tt_time)
+    % load("TSS.txt");
+    % load("N_TSS_O.txt");
+    % load("dTSS_T.txt");
+    % load("s_apprant_power_MVA_mag.txt");
+    % load("Mva_sec_abs.txt");
+    % load("Unb.txt");
+    % load("tt_time.txt");
+    % nn_track=input("Enter the TSS number for MVA profile");
+    graphics_toolkit ("gnuplot")
+    track_TSS_no=length(TSS)-length(N_TSS_O);           %% total no of TSS in track
+    ntt=length(dTSS_T(:,1));            %% total time interval of whole day operation
+    nt_track=1;
+    for n_track=1:1:track_TSS_no+2
+    
 
-if n_track<N_TSS_O(1)
-    maximum_mva(n_track)=max(s_apprant_power_MVA_mag(:,nt_track));
-    maximum_unbalance(n_track)=max(Unb(:,nt_track));
-    nt_track=nt_track+1;
+    if n_track<N_TSS_O(1)
+        maximum_mva(n_track)=max(s_apprant_power_MVA_mag(:,nt_track));
+        maximum_unbalance(n_track)=max(Unb(:,nt_track));
+        nt_track=nt_track+1;
 
-elseif n_track==N_TSS_O(1)|| n_track==N_TSS_O(2)
-    maximum_mva(n_track)=0;
-    maximum_unbalance(n_track)=0;
-else
-    maximum_mva(n_track)=max(s_apprant_power_MVA_mag(:,nt_track));
-    maximum_unbalance(n_track)=max(Unb(:,nt_track));
-    nt_track=nt_track+1;
+    elseif n_track==N_TSS_O(1)|| n_track==N_TSS_O(2)
+        maximum_mva(n_track)=0;
+        maximum_unbalance(n_track)=0;
+    else
+        maximum_mva(n_track)=max(s_apprant_power_MVA_mag(:,nt_track));
+        maximum_unbalance(n_track)=max(Unb(:,nt_track));
+        nt_track=nt_track+1;
+    end
+
+
+
+    end
+    figure;
+    figureHandle = figure;
+
+    % Get screen size from root object (0)
+    screenSize = get(0, 'ScreenSize');
+
+    % Set the figure to match the screen size
+    set(figureHandle, 'Position', screenSize);
+
+    if nn_track<N_TSS_O
+        nn_track_name=nn_track;
+        plot(tt_time/60,s_apprant_power_MVA_mag(:,nn_track))
+    elseif n_track==N_TSS_O(1)|| n_track==N_TSS_O(2)
+        outage_TSS_condition=zeros(length(s_apprant_power_MVA_mag(:,1)),1);
+        nn_track_name=nn_track;
+        plot(tt_time/60,outage_TSS_condition(:,1));
+    else
+        nn_track=nn_track-1
+        nn_track_name=nn_track+1;
+        plot(tt_time/60,s_apprant_power_MVA_mag(:,nn_track))
+    end
+
+    title(['MVA profile of TSS number ',num2str(nn_track_name) ,'.'])
+    xlabel('Time (minute)')
+    ylabel('MVA')
+
+    desired_filename = '../Plots_tTo/tTo_TSS_MVA_voltage_unbalance.png';
+    saveas(gcf, desired_filename);
+
+    save('maximum_mva_and_unbalance.mat', 'maximum_mva', 'maximum_unbalance')
 end
-
-
-
-end
-figure;
-
-if nn_track<N_TSS_O
-    nn_track_name=nn_track;
-    plot(tt_time/60,s_apprant_power_MVA_mag(:,nn_track))
-elseif n_track==N_TSS_O(1)|| n_track==N_TSS_O(2)
-    outage_TSS_condition=zeros(length(s_apprant_power_MVA_mag(:,1)),1);
-    nn_track_name=nn_track;
-    plot(tt_time/60,outage_TSS_condition(:,1));
-else
-    nn_track=nn_track-1
-    nn_track_name=nn_track+1;
-    plot(tt_time/60,s_apprant_power_MVA_mag(:,nn_track))
-end
-
-title(['MVA profile of TSS number ',num2str(nn_track_name) ,'.'])
-xlabel('Time (minute)')
-ylabel('MVA')
-maximum_mva
-maximum_unbalance
