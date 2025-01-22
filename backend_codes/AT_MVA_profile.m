dcbe00b98fb04d3d6ed2b1639e9cb85399bfdc4d
Ojaswi Chopra <ojaswichopra06@gmail.com>
2025-01-22 17:58:52 +0530

    Improved Normal Load Flow Graphs

--- backend_codes/AT_MVA_profile.m
+++ backend_codes/AT_MVA_profile.m
@@ -7,19 +7,29 @@ function AT_MVA_profile(AT_input_no, AT_mva_mag)
     %load("variable_load_flow_mum_to_ahm_each_stop.mat");
     %AT_input_no=input('Enter the AT number to see its MVA profile');
     % AT_input_no=AT_no;
+
+     % Create a figure
     figureHandle = figure;
 
     % Get screen size from root object (0)
     screenSize = get(0, 'ScreenSize');
 
-    % Set the figure to match the screen size
+    % Set the figure size to match the screen
     set(figureHandle, 'Position', screenSize);
-    
-    plot(AT_mva_mag(:,AT_input_no))
-    title(['MVA profile of AT number ',num2str(AT_input_no)])
-    xlabel('Time (sec)','FontWeight','bold')
-    ylabel('MVA','FontWeight','bold')
 
+    % Plot the MVA profile for the selected TSS
+    plot(AT_mva_mag(:, AT_input_no), 'LineWidth', 2, 'Color', [0 0.4470 0.7410]); % Blue line
+
+    % Add title and labels with enhanced styling
+    title(['MVA profile of AT number ', num2str(AT_input_no)], 'FontWeight', 'bold', 'FontSize', 14);
+    xlabel('Time (sec)', 'FontWeight', 'bold', 'FontSize', 12);
+    ylabel('MVA', 'FontWeight', 'bold', 'FontSize', 12);
+
+    % Customize axis properties for a clean appearance
+    set(gca, 'FontSize', 10, 'LineWidth', 1.2, 'Box', 'on');
+
+    % Set figure background color to white
+    set(gcf, 'Color', 'w');
 
     desired_filename = '../Plots/AT_MVA_profile.png';
     saveas(gcf, desired_filename);
--- backend_codes/TSS_MVA_profile.m
+++ backend_codes/TSS_MVA_profile.m
@@ -4,18 +4,29 @@ function TSS_MVA_profile(TSS_input_no,s_apprant_power_MVA_mag)
     format longG
     tic
     graphics_toolkit ("gnuplot")
+    % Create a figure
     figureHandle = figure;
 
     % Get screen size from root object (0)
     screenSize = get(0, 'ScreenSize');
 
-    % Set the figure to match the screen size
+    % Set the figure size to match the screen
     set(figureHandle, 'Position', screenSize);
-    
-    plot(s_apprant_power_MVA_mag(:,TSS_input_no))
-    title(['MVA profile of TSS number ',num2str(TSS_input_no)])
-    xlabel('Time (sec)','FontWeight','bold')
-    ylabel('MVA','FontWeight','bold')
+
+    % Plot the MVA profile for the selected TSS
+    plot(s_apprant_power_MVA_mag(:, TSS_input_no), 'LineWidth', 2, 'Color', [0 0.4470 0.7410]); % Blue line
+
+    % Add title and labels with enhanced styling
+    title(['MVA Profile of TSS Number ', num2str(TSS_input_no)], 'FontWeight', 'bold', 'FontSize', 14);
+    xlabel('Time (sec)', 'FontWeight', 'bold', 'FontSize', 12);
+    ylabel('MVA', 'FontWeight', 'bold', 'FontSize', 12);
+
+    % Customize axis properties for a clean appearance
+    set(gca, 'FontSize', 10, 'LineWidth', 1.2, 'Box', 'on');
+
+    % Set figure background color to white
+    set(gcf, 'Color', 'w');
+
     desired_filename = '../Plots/TSS_MVA_profile.png';
     saveas(gcf, desired_filename);
     toc
