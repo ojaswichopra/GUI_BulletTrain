b0cb382bec63cdba0ac7eec362b1eb1d83be3be1
BHARGAV PRATIM NATH <96746554+Bhargav-IITR@users.noreply.github.com>
2025-03-02 11:33:20 +0530

    OHE temp Two TSS outage module done

---
+++ Frontend/pages/OHE_tTo_up_track_dist.py
@@ -0,0 +1,74 @@
+import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
+from PIL import Image
+# Create an Oct2Py instance once
+import pages.OHE_config as OHE_config
+from oct2py import Oct2Py
+oc = Oct2Py() 
+oc.eval('cd("../OHE_temp/two_TSS_outage")') 
+import os
+
+def main():
+    st.markdown(
+            """
+        <style>
+            [data-testid="collapsedControl"] {
+                display: none
+            }
+            .title {
+                text-align: center;
+            }
+            .custom-button {
+                display: inline-block;
+                text-decoration: none;
+                padding: 4px 16px;
+                font-size: 20px;
+                color: #007BFF;
+                border: 2px solid #007BFF; /* Adding a white border */
+                border-radius: 8px;
+                transition: background-color 0.3s ease;
+                text-align: center;
+                margin: 10px 0;
+            }
+        </style>
+        """,
+            unsafe_allow_html=True,
+        )
+
+    st.markdown("<h1 class='title'>OHE Temperature at a particular distance for the entire duration of train simulation</h1>", unsafe_allow_html=True)
+    add_vertical_space(1)
+
+    dist = st.number_input("Enter the distance (in km) at which OHE temperature over entire durations of train simualtion needs to be observed", min_value=0)
+
+    if st.button("Submit"):
+        if os.path.isfile('../OHE_temp/two_TSS_outage/OHE_temp_up_track_variables_2_TSS_outage.mat'):
+            oc.eval("setenv('GNUTERM', 'gnuplot')")
+
+
+                    
+            oc.eval(f"OHE_temp_cal_up_track_dist({dist})")
+            image_path = '../Plots_OHE/OHE_temp_cal_tTo_up_track_dist.png'
+            img = Image.open(image_path)
+            st.image(img, caption="", use_column_width=True)
+
+            with open(image_path, "rb") as file:
+                btn = st.download_button(
+                    label="Download Plot",
+                    data=file,
+                    file_name="OHE_temp_cal_tTo_up_track_dist.png",  # Replace with the desired download filename
+                    mime="image/png"
+                )
+        else:
+            st.warning("Please run the Calculation!")
+
+
+if __name__ == "__main__":
+    main()
+    # if st.button("Back"):
+    #     st.switch_page("pages/Load_Flow_Output.py")
+    st.markdown(
+        f"""
+        <a href="/OHE_output_tTo_up_track" target="_self" class="custom-button">Back</a>
+        """,
+        unsafe_allow_html=True
+    )
---
+++ OHE_temp/two_TSS_outage/OHE_temp_cal_up_track_dist.m
@@ -0,0 +1,39 @@
+function OHE_temp_cal_up_track_dist(dist)
+    tic
+
+    load('OHE_temp_up_track_variables_2_TSS_outage.mat')
+
+    % dist=input('Enter the distance (in km) at which OHE temperature over entire durations of train simualtion needs to be observed: ');
+    time=[0,1:y];  % time (seconds)
+
+    graphics_toolkit ("gnuplot")
+    figureHandle = figure;
+
+    % Get screen size from root object (0)
+    screenSize = get(0, 'ScreenSize');
+
+    % Set the figure to match the screen size
+    set(figureHandle, 'Position', [100, 100, 1830, 1300]);
+
+    figure('Name','OHE temperature at a particular distance for entire durations of train simulation')
+    for ii=1:length(d_new)-1
+        if dist>=d_new(ii)/1000 && dist<d_new(ii+1)/1000
+            plot(time/60,[T_c_initial,T_c_track_up((ii-1)*y+1:ii*y)'],"Color",'r');
+            hold on
+            plot(time/60,[T_r_initial,T_r_track_up((ii-1)*y+1:ii*y)'],"Color",'g');
+            hold on
+            plot(time/60,[T_f_initial,T_f_track_up((ii-1)*y+1:ii*y)'],"Color",'b');
+            xlabel('Time (minute)')
+            ylabel('Temperature (deg cel)')
+            legend('Contact wire','Rail','Feeder wire','Location','best')
+            allAxes = findall(gcf, 'Type', 'axes'); % Find all axes in the current figure
+            set(allAxes, 'XLim', [0 time(end)/60], 'FontSize', 12, 'FontWeight', 'bold'); % Set font size and weight
+            allLines = findall(gcf, 'Type', 'line'); % Find all line objects in the figure
+            set(allLines, 'LineWidth', 1.5);          % Set the line width to 1.5
+        end
+    end
+
+    desired_filename = '../../Plots_OHE/OHE_temp_cal_tTo_up_track_dist.png';
+    saveas(gcf, desired_filename);
+    toc
+end
